c1e578be20c2bc30560e97c9bf2e7dc0eaad6d82
Administrator <1398565099@qq.com>
2017-11-23 18:51:18 +0800

    数组02 - 删除元素

---
+++ src/array/ArrayRemove.java
@@ -0,0 +1,55 @@
+package array;
+
+
+/**
+ * @author wuxiaoming
+ * @date 2017-11-23 10:27
+ */
+
+/**
+ * 数组02 - 删除元素 - 简单 - 27
+ * 给定一个数组和一个值，原地移除数组中所有给定的值，并返回新数组的长度。
+ * 不允许申请额外空间，确保空间复杂度为O(1)。
+ * 数组中的元素可以被改变，不用考虑超出新长度之后的空间遗留。
+ * <p>
+ * 比如：
+ * 给定nums = [3, 2, ,2 3]， val = 3，
+ * 你的函数应该返回length = 2， nums = [2, 2]。
+ * <p>
+ * Given an array and a value, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * <p>
+ * Example:
+ * Given nums = [3,2,2,3], val = 3,
+ * Your function should return length = 2, with the first two elements of nums being 2.
+ */
+public class ArrayRemove {
+
+    private int remove(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                index++;
+                if (i != nums.length - 1) {
+                    nums[i + 1] = nums[i];
+                }
+            }
+        }
+        return nums.length - index;
+
+    }
+
+    public static void main(String[] args) {
+        ArrayRemove ar = new ArrayRemove();
+        int remove = ar.remove(new int[]{5, 8, 1, 4, 9, 3, 19, 3, 3, 3}, 3);
+        if (remove == -1) {
+            System.out.println("\n输入的数组为空或者需要删除的元素不存在");
+        } else {
+            System.out.println("\n" + remove + "");
+        }
+    }
+}
